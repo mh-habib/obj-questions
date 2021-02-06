d3a6391dd861a155b56cbc8c0ae49325403c9d73
mh-habib <habib.tee2009@gmail.com>
2021-02-06 17:24:37 +0600

    Module-25 Object Mastering Interview Question

--- objectProperty.js
+++ objectProperty.js
@@ -5,4 +5,6 @@ var car = {type:"Fiat", model:"500", color:"white"};
 // Display some data from the object:
 car.type = 'test';
 console.log(car.type);
-//git config --global user.email "email@example.com"
+
+
+//git config --global user.email "email@example.com"//
